9937dc699928cfd03fb7112a44c746488ad9a893
thanhdanhk2004 <thanhdanhk2004@gmail.com>
2024-11-06 16:15:08 +0700

    update 06/11/2024

---
+++ saleapp/app/index.py
@@ -0,0 +1,16 @@
+from flask import render_template, request
+import dao
+from app import app
+
+
+@app.route("/")
+def index():
+    cates = dao.load_categories()
+    cate_id = request.args.get('category_id')
+    pucs = dao.load_products(cate_id=cate_id)
+
+    return render_template("index.html", categories = cates, products = pucs)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
